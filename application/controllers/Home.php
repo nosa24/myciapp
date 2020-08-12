9e8a1f8c1ff02120859298f1fb66554a16793782
nosa24 <nosa24@gmail.com>
2020-08-12 16:03:05 +0700

    menambah template header dan footer, memanggilnya melalui controller dan mencoba mengirim data ke view melalui controller dan url

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,8 +1,12 @@
 <?php
 class Home extends CI_Controller
 {
-    public function index()
+    public function index($nama = "")
     {
-        $this->load->view('home/index');
+        $data['nama'] = $nama;
+        $data['judul'] = "Halaman Home";
+        $this->load->view('templates/header', $data);
+        $this->load->view('home/index', $data);
+        $this->load->view('templates/footer');
     }
 }
