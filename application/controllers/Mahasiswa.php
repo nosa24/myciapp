c2cddae23b01b13e52f5ffa3f92297c0577a3c61
nosa24 <nosa24@gmail.com>
2020-08-12 16:59:45 +0700

    mencoba konfigurasi database dari function, construct dan global di config, autoload dan konfigurasi database di config database dengan memberi hostname, username, password dan database

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -1,11 +1,21 @@
 <?php
 class Mahasiswa extends CI_Controller
 {
+    //membuat construct
+    // public function __construct()
+    // {
+    //     parent::__construct();
+    //     $this->load->database();
+    // }
     public function index()
     {
         $data['judul'] = "Halaman Mahasiswa";
         $this->load->view('templates/header', $data);
-        $this->load->view('mahasiswa/index');
+        $this->load->view('mahasiswa/index', $data);
         $this->load->view('templates/footer');
+    }
+
+    public function insertData()
+    {
     }
 }
