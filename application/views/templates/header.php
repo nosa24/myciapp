8f741b24f885371049b0aa1352cfa905688eb45b
nosa24 <nosa24@gmail.com>
2020-08-13 10:14:02 +0700

    mengaktifkan modul url yg berada di helper file autoload, dan memberi function base_url() di tiap link di header untuk bisa berpindah page

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -21,8 +21,8 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    <a class="nav-link" href="#">Mahasiswa</a>
+                    <a class="nav-link" href="<?= base_url(); ?>">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="<?= base_url(); ?>mahasiswa">Mahasiswa</a>
                     <a class="nav-link" href="#">About</a>
                 </div>
             </div>
